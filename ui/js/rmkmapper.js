3fd579f2b0f662e65edc016b71c0edee1e3c13ac
Tambet Arak <tambet dot arak at gmail>
2017-03-20 18:51:24 +0200

    Update application to use GeoJSON

--- ui/js/rmkmapper.js
+++ ui/js/rmkmapper.js
@@ -1,28 +1,28 @@
 
 var _g = {
-	
+
 	// Marker color, category, star color and text definitions
 	// by object type
-	
+
 	markermap : {
 		"matkarada" 		: ["#00DF00", "rajad"],
 		"õpperada"		 	: ["#00A800", "rajad"],
 		"rattarada"		 	: ["#00DFE6", "rajad"],
-		"maastikusõidurada"	: ["#2B72C7", "rajad"],		
-		
-		"puhkekoht"	 		: ["#FFE0C0", "peatuskohad"],			
+		"maastikusõidurada"	: ["#2B72C7", "rajad"],
+
+		"puhkekoht"	 		: ["#FFE0C0", "peatuskohad"],
 		"lõkkekoht" 		: ["#FF9933", "peatuskohad"],
-		"telkimisala" 		: ["#FF9933", "peatuskohad", "#FFDF00"],		
+		"telkimisala" 		: ["#FF9933", "peatuskohad", "#FFDF00"],
 		"metsamaja" 		: ["#914700", "peatuskohad"],
-		"metsaonn"	 		: ["#4D2600", "peatuskohad"],			
+		"metsaonn"	 		: ["#4D2600", "peatuskohad"],
 		"looduskeskus" 		: ["#FFFF00", "peatuskohad"],
 		"teabepunkt" 		: ["#FFFF00", "peatuskohad", null, "i"],
 		"vaatetorn"	 		: ["#DC0000", "peatuskohad"],
-		
+
 		"muu"				: ["#CCCCCC", "muu"],
-		"_default"			: ["#FFFFFF"]		
+		"_default"			: ["#FFFFFF"]
 	},
-	
+
 	map					: null,
 	rmk_objects 		: null,
 	rmk_tracks			: null,
@@ -36,24 +36,24 @@ var _g = {
 //
 
 $(document).ready(function() {
-	
-	// Chain asynchronous data loading via jQuery's 
+
+	// Chain asynchronous data loading via jQuery's
 	// Deferred / Promise interface.
 
-	var loading_data = $.when(		
+	var loading_data = $.when(
 		//fetch_json("rmk-tracks.json", function(data) { _g.rmk_tracks = data; }),
-		fetch_json("rmk.json", function(data) { _g.rmk_objects = data; })
+		fetch_json("rmk.geojson", function(data) { _g.rmk_objects = data.features; })
 	);
 
 	_g.map = new google.maps.Map($("#map").get(0), {
-		center : new google.maps.LatLng(58.594, 25.016),		
+		center : new google.maps.LatLng(58.594, 25.016),
 		mapTypeId : google.maps.MapTypeId.ROADMAP,
 		zoom : 7
 	});
-	
+
 	loading_data.done(function() {
 		init();
-	});	
+	});
 	loading_data.fail(function(jqxhr, textstatus, error) {
 		throw new Error("Failed loading JSON data: " + jqxhr.status + "/" + jqxhr.statusText);
 	});
@@ -63,7 +63,7 @@ function init() {
 
 	// Called after document.ready() callback and
 	// RMK object data has been fetched and parsed.
-	
+
 	fill_filter();
 	check_viewport_size();
 	apply_rmk_objects(_g.rmk_objects, _g.map);
@@ -72,29 +72,31 @@ function init() {
 }
 
 function apply_behavior() {
-	
+
 	// Apply behavior to UI elements
-	
+
 	// jQuery UI
-	
+
 	$("#accordion")
 		.accordion({ fillSpace : true })
 		.accordion("activate", 1);
 	$("#acc_filter input").button();
-	
+
 	// Filter selection
-	
+
 	$("#acc_filter input:checkbox").change(function() {
 		var cb = $(this);
 		var type = _g.filter_type_map[cb.attr("id")];
 		var show = cb.attr("checked") ? true : false;
-		
+
 		$.each(_g.rmk_objects, function(i, o) {
-			if (o.type != type || !o.marker) return true;
-			o.marker.setVisible(show);
+			var props = o.properties;
+
+			if (props.type != type || !props.marker) return true;
+			props.marker.setVisible(show);
 		});
 	});
-	
+
 	$("#filter_sel_all").click(function() {
 		$("#acc_filter input:checkbox")
 			.attr("checked", true)
@@ -107,9 +109,9 @@ function apply_behavior() {
 			.button("refresh")
 			.change();
 	});
-		
+
 	// Compact things for small viewports
-	
+
 	$(window).resize(check_viewport_size);
 }
 
@@ -131,7 +133,7 @@ function fill_filter() {
 			styleIcon : get_styledicon(type)
 		});
 		_g.filter_type_map[cbid] = type;
-		
+
 		row +=
 			"<input type=\"checkbox\" id=\"" + cbid + "\" />" +
 			"<label for=\"" + cbid + "\">" + "<img src=\"" + marker.getIcon() + "\" \>&nbsp;" + type + "</label>" +
@@ -139,7 +141,7 @@ function fill_filter() {
 		div.append(row);
 		i++;
 	});
-	
+
 	div.find(":checkbox").attr("checked", true);
 }
 
@@ -148,7 +150,7 @@ function fill_filter() {
 //
 
 function fetch_json(url, cb) {
-	return $.get(url, null, function(data, textstatus) {		
+	return $.get(url, null, function(data, textstatus) {
 		if (data) {
 			if (typeof(data) == "string") // if sent as text/plain
 				data = JSON.parse(data);
@@ -162,50 +164,54 @@ function fetch_json(url, cb) {
 
 function apply_rmk_objects(rmk_objects, map) {
 
-	// Apply objects to map. The objects array is modified -
-	// Marker and InfoWindow instances are added to each object
-	// with coordinates.
+	// Apply objects (that are also valid GeoJSON Features) to map. The objects
+	// array is modified - Marker and InfoWindow instances are added to each
+	// object with coordinates.
 
 	$.each(rmk_objects, function(i, o) {
-		var o = rmk_objects[i];
-		var marker = o.marker;
-		var infow = o.infowindow;		
+		var props = o["properties"];
+		var coords = o["geometry"]["coordinates"];
+		var coord_lon = coords[0];
+		var coord_lat = coords[1];
+
+		var marker = props.marker;
+		var infow = props.infowindow;
 		var regio_href;
 		var campicons;
-		
-		if (!o.coord_lat || !o.coord_lon)
+
+		if (!coord_lat || !coord_lon)
 			return true;
-		
+
 		if (!marker) {
 			marker = new StyledMarker({
-				styleIcon : get_styledicon(o.type),
-				position : new google.maps.LatLng(o.coord_lat, o.coord_lon),
+				styleIcon : get_styledicon(props.type),
+				position : new google.maps.LatLng(coord_lat, coord_lon),
 				map : map
 			});
-			o.marker = marker;
+			props.marker = marker;
 		}
-		
-		if (!infow) {		
+
+		if (!infow) {
 			campicons =
 				"<div class=\"campicons\">" +
-				(o.has_tenting ? 
+				(props.has_tenting ?
 					"<img src=\"ui/img/icon-tent-32.png\" alt=\"Telkimine\"" +
 					"title=\"Telkimisvõimalus\" />" : "") +
-				(o.has_firesite ? 
+				(props.has_firesite ?
 					"<img src=\"ui/img/icon-campfire-32.png\" alt=\"Lõkkekoht\"" +
 					"title=\"Kattega lõkkekoht\" />" : "") +
 				"</div>";
-			regio_href = 
+			regio_href =
 				"http://kaart.otsing.delfi.ee/index.php?id=1&bbox=" +
-				o.coord_x + "," + o.coord_y + "," + o.coord_x + "," + o.coord_y;				
+				props.coord_x + "," + props.coord_y + "," + props.coord_x + "," + props.coord_y;
 			infow = new google.maps.InfoWindow({
-				content : 
-					"<div class=\"iwcontent\">" + 
-					"<b>" + o.name + "</b><ul>" +
-					(o.location  ? "<li>" + o.location  + "</li>" : "") +
-					(o.equipment ? "<li>" + o.equipment + "</li>" : "") +
-					(o.sights    ? "<li>" + o.sights    + "</li>" : "") +						
-					"</ul><a href=\"" + o.href + "\">RMK</a>" + " | " +
+				content :
+					"<div class=\"iwcontent\">" +
+					"<b>" + props.name + "</b><ul>" +
+					(props.location  ? "<li>" + props.location  + "</li>" : "") +
+					(props.equipment ? "<li>" + props.equipment + "</li>" : "") +
+					(props.sights    ? "<li>" + props.sights    + "</li>" : "") +
+					"</ul><a href=\"" + props.href + "\">RMK</a>" + " | " +
 					"<a href=\"" + regio_href + "\">Regio atlas</a>" +
 					campicons + "</div>"
 			});
@@ -215,7 +221,7 @@ function apply_rmk_objects(rmk_objects, map) {
 				infow.open(map, marker);
 				_g.infow_last = infow;
 			});
-			o.infowindow = infow;
+			props.infowindow = infow;
 		}
 	});
 }
@@ -226,33 +232,33 @@ function apply_rmk_tracks(rmk_tracks, map) {
 		var polyline;
 		var infow;
 		var anch;
-		
+
 		$.each(o.waypoints, function(i, ll) {
 			path.push(new google.maps.LatLng(ll[0], ll[1]));
 		});
-		
-		polyline = new google.maps.Polyline({			
+
+		polyline = new google.maps.Polyline({
 			path : path,
 			map : map,
 			strokeColor: "#37A42C",
 			strokeOpacity: 0.6,
 		});
 		infow = new google.maps.InfoWindow({
-			content : 
-				"<div class=\"iwcontent\">" + 
+			content :
+				"<div class=\"iwcontent\">" +
 				"<b>" + o.name + " rada</b><ul>" +
 				"</div>"
 		});
 		anch = new google.maps.MVCObject;
-		
+
 		google.maps.event.addListener(polyline, 'click', function(e) {
 			anch.set("position", e.latLng);
 			if (_g.infow_last)
-				_g.infow_last.close();			
+				_g.infow_last.close();
 			infow.open(map, anch);
 			_g.infow_last = infow;
-		});		
-		
+		});
+
 		o.polyline = polyline;
 	});
 }
@@ -268,7 +274,7 @@ function check_viewport_size() {
 	else {
 		$("#acc_filter").css("padding", "");
 	}
-	
+
 	if ($(window).width() > 1400) {
 		$("body").css("padding-left", "4em");
 		$("body").css("padding-right", "4em");
@@ -276,23 +282,23 @@ function check_viewport_size() {
 	else {
 		$("body").css("padding-left", "");
 		$("body").css("padding-right", "");
-	}	
+	}
 }
 
 function get_styledicon(type) {
 
 	// Return a new StyledIcon based on object type.
 	// Generated icons are cached.
-	
+
 	var color 		= _g.markermap[type][0] || _g.markermap["_default"][0];
 	var starcolor	= _g.markermap[type][2] || "";
 	var text		= _g.markermap[type][3] || "";
 	var si			= _g.cache_styledicon[type];
-	
+
 	if (si) return si;
 
 	si = new StyledIcon(
-		text ? StyledIconTypes.BUBBLE : StyledIconTypes.MARKER, 
+		text ? StyledIconTypes.BUBBLE : StyledIconTypes.MARKER,
 		{color : color, starcolor : starcolor, text : text}
 	);
 	_g.cache_styledicon[type] = si;
